c4fcf2141473386ee57b6d4f496728bb41d6cdc8
Sunny <swarnendughosh05@gmail.com>
2024-08-12 22:52:47 +0530

    Visit nav bar 80% completed

--- components/VistNavBar.tsx
+++ components/VistNavBar.tsx
@@ -1,35 +1,53 @@
 "use client"
 
 import Link from "next/link"
-
 import { cn } from "@/lib/utils"
 import { Montserrat } from "next/font/google";
-import { WholeWord } from "lucide-react";
 import Image from "next/image";
+import { Search } from "lucide-react";
 
 const font = Montserrat({
     weight: "600",
     subsets: ["latin"]
 });
 
-
-export const  VisitnavBar = () => {
-     return (
-         <nav className="p-4 pt-3 bg-transparent flex items-center justify-between border-b-2 bg-zinc-900 border-blue-950">
-            <div className="w-6 h-8">
-                <Link href="/" className="flex items-center">
-                <Image 
-                alt="India"
-                src='/india.png'
-                width={50}
-                height={30}
-                className="lg:ml-14 md:ml-14"
-                />
-                <h1 className={cn("text-xl font-bold text-white ml-1.5", font.className)}>Visit India</h1>
-                
+export const VisitnavBar = () => {
+    return (
+        <nav className="p-4 pt-3 bg-transparent flex items-center justify-between border-b-2 bg-zinc-900 border-blue-950">
+            <div className="flex items-center">
+                <Link href="/visit" className="flex items-center">
+                    <Image 
+                        alt="India"
+                        src='/india.png'
+                        width={30}
+                        height={30}
+                        className="lg:ml-14 md:ml-14"
+                    />
+                    <h1 className={cn("text-xl font-bold text-white mt-2 ml-2", font.className)}>Visit India</h1>
                 </Link>
             </div>
-         </nav>
-     )
+
+            <div className="flex-grow flex justify-center items-center space-x-6 mr-52">
+                <p className="text-white whitespace-nowrap">
+                    Places to Visit
+                </p>
+                <p className="text-white whitespace-nowrap">
+                    Things to Do
+                </p>
+                <p className="text-white whitespace-nowrap">
+                    Find Accommodation
+                </p>
+                <p className="text-white whitespace-nowrap">
+                    Plan Your Trip
+                </p>
+            </div>
+            
+            <span className="flex items-center space-x-2">
+                <Search className="text-white mr-32"/>
+                
+            </span>
+        </nav>
+    )
 }
-export default VisitnavBar
+
+export default  VisitnavBar
