0f4b2208f26f30627fa52d2af06cc1a4f87992fb
Sunny <swarnendughosh05@gmail.com>
2024-08-17 01:24:30 +0530

    visit page demo card added for tets

--- components/VisitContent.tsx
+++ components/VisitContent.tsx
@@ -1,9 +1,32 @@
+"use client";
 import React from 'react'
+ 
+import { useRef, useState } from "react";
+import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+import { DirectionAwareHover } from './ui/direction-aware-hover';
+
+const ramdom = [
+  {
+    label: "In the mountain",
+    imageUrl: "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "Indian mountains are best"
+  }
+]
+
 
 const VisitContent = () => {
+  
   return (
-    <div className='text-3xl'>
-         Must-do Indian experiences
+    <div className="h-[40rem] relative  flex items-center justify-center">
+      {ramdom.map((item, i) => (
+      <DirectionAwareHover key={i} imageUrl={item.imageUrl}>
+        <p className="font-bold text-xl">{item.label}</p>
+        <p className="font-normal text-sm">{item.description}</p>
+      </DirectionAwareHover>
+   ))}   
+  
     </div>
   )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['aceternity.com'], // Add the hostname here
+      domains: ['aceternity.com',
+                "images.unsplash.com",
+
+      ], // Add the hostname here
     },
   };
   
