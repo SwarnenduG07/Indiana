7bf9f24fc93802e1c369e2819ee96b4f52c6ab74
Sunny <swarnendughosh05@gmail.com>
2024-08-19 01:36:09 +0530

    fixed

--- components/visit/ThingsToDo.tsx
+++ components/visit/ThingsToDo.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ThingsToDoVisit(){
   return (
-    <div className='text-4xl font-bold ml-36'>
+    <div className='text-4xl font-bold text-white ml-36'>
         Things To Do
     </div>
   )
