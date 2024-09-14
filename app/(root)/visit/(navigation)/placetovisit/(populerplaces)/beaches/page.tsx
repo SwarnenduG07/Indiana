5c11e561d4ad1960b62c9705965e44488cbc0a03
Sunny <swarnendughosh05@gmail.com>
2024-09-15 00:22:24 +0530

    Beaches init

--- app/(root)/visit/(navigation)/placetovisit/(populerplaces)/beaches/page.tsx
+++ app/(root)/visit/(navigation)/placetovisit/(populerplaces)/beaches/page.tsx
@@ -1,8 +1,17 @@
+import Testimonial from '@/components/visit/Testimonial'
 import React from 'react'
 
 const Beaches = () => {
   return (
-    <div>Beaches</div>
+    <div>
+      <h1 className='max-w-xl ml-44 mt-72 text-2xl'>
+      With around 15,000 kilometres of coastline and more than 25 marine reserves, ocean-scented scenery is entwined with New Zealand’s outdoorsy way of life.
+      </h1>
+      <div>
+         <Testimonial />
+      </div>
+    </div>
+
   )
 }
 
---
+++ components/visit/Testimonial.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Testimonial = () => {
+  return (
+    <div>
+        <div>
+            <h1 className='font-bold text-4xl ml-44 mt-20 '>
+               Popular North Island beaches
+            </h1> 
+            <h1 className='max-w-4xl ml-44 mt-8'>
+            Pack your sandals, shorts and sunblock, and always be prepared for spontaneous picnics. Wherever you are in the North Island, you’re never far from a day at the beach.
+            </h1>       
+            <div>
+                {/* //IMAGE WOULD BE ADDED */}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Testimonial
